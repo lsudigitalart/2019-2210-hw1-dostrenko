e0704b5a07b1c278a631e92585052d8950341708
Derick Ostrenko <dostrenko@gmail.com>
2019-08-29 13:58:46 -0500

    added second cricle

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 function setup() {
   createCanvas(800, 600);
   // background(100);
+  fill(255);
   ellipse(width / 2, height / 2, 20, 50);
+  fill(0);
+  ellipse(0, 0, 100, 100);
 }
 
 function draw() {
